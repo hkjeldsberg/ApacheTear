a4b89bab6c5c92416e7fd9748a0f307895641a99
Henrik Kjedsberg <henrik.kjeldsberg@live.no>
2026-06-24 10:16:55 +0200

    Add icons

--- app/icon.tsx
+++ app/icon.tsx
@@ -12,6 +12,7 @@ export default function Icon(): ImageResponse {
         height="32"
         xmlns="http://www.w3.org/2000/svg"
       >
+        <rect x="0" y="0" width="32" height="32" rx="7" ry="7" fill="white" />
         {/* Teardrop: pointed tip at top, round base at bottom */}
         <path
           d="M16 2 C 8 10 6 16 6 22 A 10 10 0 0 0 26 22 C 26 16 24 10 16 2 Z"
